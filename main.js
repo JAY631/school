d05b402c37e5a0ba7d43f4f25a25fb31b6a0954e
JAY631 <myizambekov.baiel9@gmail.com>
2023-04-29 22:29:15 +0600

    news

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+let open = document.getElementById("open");
+let close = document.getElementById("close");
+let answer = document.getElementById("answer");
+let leave = document.getElementById("leave");
+let padding = document.querySelector(".comments__content");
+open.onclick = function () {
+  answer.style.display = "block";
+  leave.style.display = "none";
+  // padding.style.paddingBottom = "4em";
+};
+close.onclick = function () {
+  answer.style.display = "none";
+  leave.style.display = "block";
+  // padding.style.paddingBottom = "4em";
+};
+
 AOS.init();
 
 Fancybox.bind("#gallery a", {
